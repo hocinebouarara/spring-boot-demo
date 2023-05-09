adc1439adb3cfe4f9e00e40a7be64f04aaedfd13
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-09 07:28:06 +0100

    added the first request mapping

--- src/main/java/com/hocinebouarara/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/hocinebouarara/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,20 @@ package com.hocinebouarara.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
+    @GetMapping("")
+    public String greet(){
+        return "Hello world!!";
+    }
 }
