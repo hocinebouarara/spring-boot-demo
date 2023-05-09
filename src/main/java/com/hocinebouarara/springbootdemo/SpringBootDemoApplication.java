4b9cf5799dc84438fbb7fd02cb054d6599ca7da4
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-09 08:03:05 +0100

    create a class for the return format

--- src/main/java/com/hocinebouarara/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/hocinebouarara/springbootdemo/SpringBootDemoApplication.java
@@ -2,11 +2,10 @@ package com.hocinebouarara.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -21,5 +20,37 @@ public class SpringBootDemoApplication {
         return new GreetResponse("Hello world!");
     }
 
-    record GreetResponse(String greet){}
+    //record GreetResponse(String greet){}
+
+    class GreetResponse{
+        private final String greet;
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+    }
 }
