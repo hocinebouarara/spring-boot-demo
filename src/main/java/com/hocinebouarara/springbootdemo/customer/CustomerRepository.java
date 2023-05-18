93a13e636d529c2fb6b0f6f60f15d1fecef2848c
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-18 23:16:16 +0100

    create customer repo with jpa for crud ops

---
+++ src/main/java/com/hocinebouarara/springbootdemo/customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.hocinebouarara.springbootdemo.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
