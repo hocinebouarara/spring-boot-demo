be8100c0597ee63a5b87269fdc4f503241b8c8f7
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-19 05:44:55 +0100

    create the method of addCustomer but before we take a test if the email is taken

--- src/main/java/com/hocinebouarara/springbootdemo/customer/CustomerService.java
+++ src/main/java/com/hocinebouarara/springbootdemo/customer/CustomerService.java
@@ -1,6 +1,6 @@
 package com.hocinebouarara.springbootdemo.customer;
 
-import com.hocinebouarara.springbootdemo.exception.ResourceNotFount;
+import com.hocinebouarara.springbootdemo.exception.ResourceNotFountException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +19,26 @@ public class CustomerService {
     }
     public Customer getCustomer(Integer id) {
         return customerDao.selectCustomerById(id)
-                .orElseThrow(()->new ResourceNotFount(
+                .orElseThrow(()->new ResourceNotFountException(
                         "Customer with id %s not found".formatted(id)));
+    }
+
+    public void addCustomer(
+            CustomerRegistrationRequest customerRegistrationRequest){
+
+        // check if the email exist
+        if(customerDao.existPersonWithEmail(customerRegistrationRequest.email())){
+            throw new ResourceNotFountException("email already taken");
+        }
+        // add
+
+        Customer customer = new Customer();
+
+        customer.setName(customerRegistrationRequest.name());
+        customer.setEmail(customerRegistrationRequest.email());
+        customer.setAge(customerRegistrationRequest.age());
+
+        customerDao.insertCustomer(customer);
+
     }
 }
