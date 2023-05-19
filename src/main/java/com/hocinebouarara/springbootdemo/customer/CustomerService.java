f96c02915df32224044c379e0fa93e3e7dfa853d
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-19 07:11:09 +0100

    create the service of updating customers

--- src/main/java/com/hocinebouarara/springbootdemo/customer/CustomerService.java
+++ src/main/java/com/hocinebouarara/springbootdemo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.hocinebouarara.springbootdemo.customer;
 
+import com.hocinebouarara.springbootdemo.exception.RequestValidationException;
 import com.hocinebouarara.springbootdemo.exception.ResourceNotFountException;
+import com.sun.jdi.request.DuplicateRequestException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -53,5 +55,40 @@ public class CustomerService {
         //delete
         customerDao.deleteCustomerById(customerId);
 
+    }
+
+    public void updateCustomer(Integer customerId,
+                               CustomerRegistrationRequest updateRequest){
+        Customer customer = getCustomer(customerId);
+
+        boolean changes = false;
+
+        if (updateRequest.name() != null && !updateRequest.name().equals(customer.getName())){
+            customer.setName(updateRequest.name());
+            changes = true;
+        }
+
+        if (updateRequest.email() != null && !updateRequest.email().equals(customer.getEmail())){
+            if (customerDao.existPersonWithEmail(updateRequest.email())){
+                throw  new DuplicateRequestException(
+                        "email already taken"
+                );
+            }
+            customer.setEmail(updateRequest.email());
+            changes = true;
+        }
+
+        if (updateRequest.age() != null && !updateRequest.age().equals(customer.getAge())){
+            customer.setAge(updateRequest.age());
+            changes = true;
+        }
+
+        if (!changes){
+            throw new RequestValidationException(
+                    "no data changes found"
+            );
+        }
+
+        customerDao.updateCustomer(customer);
     }
 }
