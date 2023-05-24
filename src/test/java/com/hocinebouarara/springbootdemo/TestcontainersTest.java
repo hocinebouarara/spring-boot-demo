6511d1017f67d8be5300ee226d78ebac50965b19
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-24 07:13:16 +0100

    extends the test container abstract class for accessing

--- src/test/java/com/hocinebouarara/springbootdemo/TestcontainersTest.java
+++ src/test/java/com/hocinebouarara/springbootdemo/TestcontainersTest.java
@@ -9,50 +9,15 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static com.hocinebouarara.springbootdemo.AbstractTestContainers.postgresSQLContainer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
-public class TestcontainersTest {
-
-    @Container
-    private static PostgreSQLContainer<?> postgresSQLContainer
-            = new PostgreSQLContainer<>("postgres:latest")
-            .withDatabaseName("hocine-dao-unit-test")
-            .withUsername("postgres")
-            .withPassword("hocine09");
-
-    @DynamicPropertySource
-    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
-        registry.add(
-                "spring.datasource.url",
-                () -> postgresSQLContainer.getJdbcUrl()
-        );
-        registry.add(
-                "spring.datasource.username",
-                () -> postgresSQLContainer.getUsername()
-        );
-        registry.add(
-                "spring.datasource.password",
-                () -> postgresSQLContainer.getPassword()
-        );
-
-    }
-
+public class TestcontainersTest extends AbstractTestContainers{
     @Test
     void canStartPostgresDB() {
         assertThat(postgresSQLContainer.isRunning()).isTrue();
         assertThat(postgresSQLContainer.isCreated()).isTrue();
     }
 
-    @Test
-    void canApplyDbMigrationWithFlyway(){
-        Flyway flyway = Flyway.configure().dataSource(
-                postgresSQLContainer.getJdbcUrl(),
-                postgresSQLContainer.getUsername(),
-                postgresSQLContainer.getPassword()
-        ).load();
-
-        flyway.migrate();
-        System.out.println();
-    }
 }
