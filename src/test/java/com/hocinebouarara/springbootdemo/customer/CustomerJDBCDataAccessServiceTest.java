454c445424791e571c97713cada9635349a6699e
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-25 05:23:25 +0100

    init the Test Class of JDBC Data Access

---
+++ src/test/java/com/hocinebouarara/springbootdemo/customer/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,41 @@
+package com.hocinebouarara.springbootdemo.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerJDBCDataAccessServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void selectAllCustomers() {
+    }
+
+    @Test
+    void selectCustomerById() {
+    }
+
+    @Test
+    void insertCustomer() {
+    }
+
+    @Test
+    void existPersonWithEmail() {
+    }
+
+    @Test
+    void existsPersonWithId() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+}
